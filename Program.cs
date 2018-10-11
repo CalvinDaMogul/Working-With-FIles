4dad97bbb0dd12bc83761139370c94d235da10ab
Calvin J Foster <cal.j.foster@gmail.com>
2018-10-11 00:01:36 -0500

    second commit

--- Program.cs
+++ Program.cs
@@ -9,6 +9,11 @@ namespace WorkingWithFiles
             var path = @ "c:\somefile.jpg";
 
             File.Copy(@"c:\temp\myfile.jpg", @);
+            File.Delete(path);
+            if (FileStyleUriParser.Exists(path));
+            {
+                
+            }
             
             Console.WriteLine("Hello World!!!");
         }
