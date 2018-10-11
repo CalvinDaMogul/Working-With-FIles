538367b405b58e849fe76236d9cd497c6ae3cd7e
Calvin J Foster <cal.j.foster@gmail.com>
2018-10-10 23:55:45 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WorkingWithFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @ "c:\somefile.jpg";
+
+            File.Copy(@"c:\temp\myfile.jpg", @);
+            
+            Console.WriteLine("Hello World!!!");
+        }
+    }
+}
